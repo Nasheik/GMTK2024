9401fa91c9c0635ab15833ecac6022a23a8c1578
Kishan <kshnjshptl@gmail.com>
2024-11-05 15:16:28 -0500

    no message

--- Assets/Scripts/Editor/Builder.cs
+++ Assets/Scripts/Editor/Builder.cs
@@ -10,7 +10,7 @@ public class WebGLBuilder
     [MenuItem("MyTools/Build")]
     public static void BuildGame()
     {
-        string path = Application.dataPath + "..\\..\\Builds\\";
+        string path = Application.dataPath + "/../../Builds/";
         string[] scenes = new string[SceneManager.sceneCountInBuildSettings];
         for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
         {
@@ -19,6 +19,6 @@ public class WebGLBuilder
         Debug.Log("PATH:" + path);
         //BuildPipeline.BuildPlayer(scenes, path + "Web\\", BuildTarget.WebGL, BuildOptions.Development);
         // BuildPipeline.BuildPlayer(scenes, path + "Linux\\CTS.x86_64", BuildTarget.StandaloneLinux64, BuildOptions.None);
-        BuildPipeline.BuildPlayer(scenes, path + "Windows\\CTS.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, path + "Windows/CTS.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 }
